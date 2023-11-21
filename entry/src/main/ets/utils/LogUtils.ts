2d501e120f4be15673d991e9e65b4af3519a2824
WinWang <wangchangsheng026417@gtjas.com>
2023-11-21 21:42:59 +0800

    添加网络请求封装

--- entry/src/main/ets/utils/LogUtils.ts
+++ entry/src/main/ets/utils/LogUtils.ts
@@ -1,7 +1,27 @@
+import hilog from '@ohos.hilog'
 
+const LogTag = "tag"
+const StringFormat = "%{public}s"
 
-export class LoaUtils{
+class LogUtils {
+  logDebug(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+    hilog.debug(domain, tag, stringFormat, logInfo);
+  }
 
+  logInfo(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+    hilog.info(domain, tag, stringFormat, logInfo);
+  }
 
+  logWarn(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+    hilog.warn(domain, tag, stringFormat, logInfo);
+  }
+
+  logError(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+    hilog.error(domain, tag, stringFormat, logInfo);
+  }
 }
+
+let logUtils = new LogUtils()
+
+export default logUtils as LogUtils
 
