72b860e480ec361f59d216bb8fbd061bdb318708
winwang <1256242870@qq.com>
2023-11-22 00:00:51 +0800

    添加网络请求封装-完善基础请求

--- entry/src/main/ets/utils/LogUtils.ts
+++ entry/src/main/ets/utils/LogUtils.ts
@@ -3,25 +3,21 @@ import hilog from '@ohos.hilog'
 const LogTag = "tag"
 const StringFormat = "%{public}s"
 
-class LogUtils {
-  logDebug(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+export class LogUtils {
+  static logDebug(tag: string = LogTag, domain: number = 0x0000, stringFormat: string = StringFormat, ...logInfo: any[]) {
     hilog.debug(domain, tag, stringFormat, logInfo);
   }
 
-  logInfo(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+  static logInfo(tag: string = LogTag, domain: number = 0x0000, stringFormat: string = StringFormat, ...logInfo: any[]) {
     hilog.info(domain, tag, stringFormat, logInfo);
   }
 
-  logWarn(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+  static logWarn(tag: string = LogTag, domain: number = 0x0000, stringFormat: string = StringFormat, ...logInfo: any[]) {
     hilog.warn(domain, tag, stringFormat, logInfo);
   }
 
-  logError(domain: number = 0x0000, tag: string = LogTag, stringFormat: string = StringFormat, ...logInfo: any[]) {
+  static logError(tag: string = LogTag, domain: number = 0x0000, stringFormat: string = StringFormat, ...logInfo: any[]) {
     hilog.error(domain, tag, stringFormat, logInfo);
   }
 }
-
-let logUtils = new LogUtils()
-
-export default logUtils as LogUtils
 
